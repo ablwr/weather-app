f6429ddf57b75f80e4e5fbb6e01d06396370a370
kylefdoherty <kyledoherty22@gmail.com>
2014-07-29 15:46:16 -0400

    added forecast api and current weather method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ActiveRecord::Base
-
-
 	validates_presence_of :name, :email
 	validates :phone, :presence => true,
                  		:numericality => true,
                  		:length => { :minimum => 10, :maximum => 15 }
+
+  ForecastIO.configure do |configuration|
+    configuration.api_key = '2228ceef0d8a1294ece6fd4935a085bb'
+  end
+
+  def current_weather
+    lat = 40.7127
+    long = -74.0059
+    ForecastIO.forecast(lat, long).currently.summary
+  end 
 
 end
