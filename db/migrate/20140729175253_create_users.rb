bfa649efb90600c834fdbd1df842f4db044cb072
kylefdoherty <kyledoherty22@gmail.com>
2014-07-29 16:08:02 -0400

    started lat and long

--- db/migrate/20140729175253_create_users.rb
+++ db/migrate/20140729175253_create_users.rb
@@ -1,9 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string :name
-    	t.integer :phone
-    	t.string :email
+    	t.string   :name
+    	t.integer  :phone
+    	t.string   :email
+      t.float    :latitude
+      t.float    :longitude
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 20140729175253) do
     t.string   "name"
     t.integer  "phone"
     t.string   "email"
+    t.float    "latitude"
+    t.float    "longitude"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
