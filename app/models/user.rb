24113b5c0014a1f8c0ee84a3c5285cff8a885e5e
ablwr <ashley.blewer@gmail.com>
2014-07-29 16:58:26 -0400

    removed phone required field to push

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
 	validates_presence_of :name, :email
-	validates :phone, :presence => true,
-                 		:numericality => true,
-                 		:length => { :minimum => 10, :maximum => 15 }
+	# validates :phone, :presence => true,
+ #                 		:numericality => true,
+ #                 		:length => { :minimum => 10, :maximum => 15 }
 
   ForecastIO.configure do |configuration|
     configuration.api_key = '2228ceef0d8a1294ece6fd4935a085bb'
