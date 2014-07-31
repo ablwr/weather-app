3598377aa181f55d7d465c59b5a84dbd11299398
ldemarest <demarest.luke@gmail.com>
2014-07-31 16:54:14 -0400

    updates weather  methods and  show view

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -11,46 +11,43 @@ module Weather
     def currently
       forecast.currently
     end
+
+    def minutely
+      forecast.minutely.data[48]
+    end
+
+    def minutely_summary
+      forecast.minutely.summary
+    end
+
+    def minutely_icon
+      forecast.minutely.icon
+    end
     
     def hourly
-      forecast.hourly
+      forecast.hourly.data[48]
+    end
+
+    def hourly_summary
+      forecast.hourly.summary
+    end 
+
+    def hourly_icon
+      forecast.hourly.icon
     end
 
     def daily
-      forecast.daily
+      forecast.daily.data[48]
     end
 
-    def summary
-      summary
-    end 
-
-    def icon
-      icon
+    def daily_summary
+      forecast.daily.summary
     end
 
-    # def all_data
-    #   # data = {temp: forecast.currently.temp,
-    #   # apparent_temp: forecast.currently.apparentTemperature,
-    #   # cloud_cover: forecast.currently.cloudCover,
-    #   # dew_point: forecast.currently.dewPoint,
-    #   # humidity: forecast.currently.humidity,
-    #   # icon: forecast.currently.icon,
-    #   # ozone: forecast.currently.ozone,
-    #   # precipitation_intensity: forecast.currently.precipIntensity,
-    #   # precipitation_probability: forecast.currently.precipProbability,
-    #   # precipitation_type: forecast.currently.precipType,
-    #   # pressure: forecast.currently.pressure,
-    #   # time: forecast.currently.time,
-    #   # wind_bearing: forecast.currently.windBearing,
-    #   # wind_speed: forecast.currently.windSpeed,
-    #   # sunrise_time: forecast.currently.sunriseTime,
-    #   # sunset_time: forecast.currently.sunsetTime
-    #   # } 
-    #   data
-    # end
-
+    def daily_icon
+      forecast.daily.icon
+    end
 
   end 
-
 
 end 
