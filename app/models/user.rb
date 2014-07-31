92a40ae2f0d8cb96065bcbbfa848661de763b321
kylefdoherty <kyledoherty22@gmail.com>
2014-07-31 10:06:39 -0400

    got clockwork working

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,43 +16,16 @@ class User < ActiveRecord::Base
     text
   end
 
-  # handler receives the time when job is prepared to run in the 2nd argument
-  # handler do |job, time|
-  #   puts "Running #{job}, at #{time}"
-  # end
-
-  every(30.seconds, 'sms', :thread => true) {
-    User.all.each do |user|
-      # binding.pry
-      if user.time.utc.hour == Time.now.hour && user.time.min == Time.now.utc.min
-        user.sms 
-        puts "Yep for #{user.name}"
-      else
-        puts "Nope! for #{user.name}"
+    every(1.minute, 'sms') {
+      users = User.all.select  {|user| user.time.hour == Time.now.hour && user.time.min == Time.now.min} 
+      if !users.empty?
+        users.each do |user|
+          user.sms
+        end 
+      else 
+        puts "nope"
       end 
-    end 
-
-
-    
-  }
-  # every(3.minutes, 'less.frequent.job')
-  # every(1.hour, 'hourly.job')
-
-  # every(1.day, 'midnight.job', :at => '00:00')
-
-
-  # ever day at self.time if perciptation == 1
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
   def sms
